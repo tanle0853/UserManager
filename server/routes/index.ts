18c90bdbd16e90cf37d6623ceb2c91f45f33fcf1
tanle0853 <tanlt2@rikkeisoft.com>
2023-08-16 23:06:00 +0900

    add

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -15,4 +15,21 @@ router.post("/user", async (req, res) => {
   res.json(savedUser);
 });
 
+router.get("/user/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
+});
+
+router.put("/user/:id", async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(updatedUser);
+});
+
+router.delete("/user/:id", async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+  res.json(user);
+});
+
 export default router;
---
+++ src/components/userDetail.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="col-md-4 offset-md-4">
+    <form @submit.prevent="handleUpdate()" class="card card-body">
+      <h1 class="card-title my-3 text-center">Cap nhat</h1>
+
+      <input
+        type="text"
+        v-model="currentuser.title"
+        class="form-control mb-3"
+      />
+
+      <textarea
+        v-model="currentuser.description"
+        class="form-control mb-3"
+      ></textarea>
+
+      <button class="btn btn-primary">Luu</button>
+    </form>
+
+    <button @click="handleDelete()" class="btn btn-danger my-4">Xoa</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { user } from "@/interfaces/user";
+import { deleteuser, getuser, updateuser } from "@/services/userService";
+
+export default defineComponent({
+  name: "user",
+  data() {
+    return {
+      currentuser: {} as user,
+    };
+  },
+  methods: {
+    async loaduser(id: string) {
+      try {
+        const { data } = await getuser(id);
+        this.currentuser = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async handleUpdate() {
+      try {
+        if (typeof this.$route.params.id === "string") {
+          await updateuser(this.$route.params.id, this.currentuser);
+          this.$router.push("/");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async handleDelete() {
+      try {
+        if (typeof this.$route.params.id === "string") {
+          deleteuser(this.$route.params.id);
+          this.$router.push("/");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  mounted() {
+    if (typeof this.$route.params.id === "string") {
+      this.loaduser(this.$route.params.id);
+    }
+  },
+});
+</script>
