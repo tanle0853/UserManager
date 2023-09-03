b9db54d50148fecb626ce2075af3ff6e05be4306
tanle0853 <tanlt2@rikkeisoft.com>
2023-09-04 01:09:28 +0900

    add comit

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/User";
+import { Password } from '../password';
+
 
 const router = Router();
 
@@ -39,17 +42,32 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username });
 
-    if (!user || user.password !== password) {
+    if (!user) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    // Thực hiện logic đăng nhập ở đây, ví dụ như tạo session hoặc JWT token
+    if (password === undefined) {
+      return res.status(401).json({ message: "Password is undefined" });
+    }
 
-    res.json({ message: "Login successful", user });
+    const isPasswordValid = await Password.verify(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+    const secretKey = process.env.JWT_SECRET;
+
+    // Tiếp tục xử lý đăng nhập thành công
+    const token = jwt.sign({ userId: user._id }, secretKey, {
+      expiresIn: "1h",
+    });
+
+    res.json({ message: "Login successful", user, token });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "An error occurred" });
   }
 });
+
 
 // API đăng xuất
 router.post("/logout", (req, res) => {
